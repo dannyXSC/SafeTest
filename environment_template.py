e550423a6c21f86c6b5efa472b974ff18313b0fb
RacerZ-fighting <2245945825@qq.com>
2024-08-30 16:37:16 +0800

    minor change

---
+++ environment_template.py
@@ -0,0 +1,11 @@
+driver_path = "/Users/racerz/Downloads/chromedriver-mac-arm64/chromedriver"
+main_page = "https://lsem.fudan.edu.cn/fd_aqks_new/examProgress/examOnline/examProgressOnlineIndex"
+auth_url = "https://uis.fudan.edu.cn/authserver/login"
+
+dataset_path = "./asset/dataset/index.html"
+cookie_path = "./asset/cookie.txt"
+question_path = "./asset/questions.json"
+
+if_load_cookie = False
+
+input_wait_time = 20
